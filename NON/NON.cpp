c67704007c66d3c73a31e0bbaa0a7ee1aa12a625
Łukasz Tabor <l.tabor@partner.samsung.com>
2019-09-10 19:17:44 +0200

    NON wip

--- NON/NON.cpp
+++ NON/NON.cpp
@@ -4,15 +4,56 @@ class Obj {
 public:
     int*fields_prt;
     int size;
-    int begin;
-    int end;
+    int min_start;
+    int max_start;
     Obj* prev;
     Obj* next;
 
-    void setNewBegin(int beg);
-    void setNewEnd(int end);
+    void setNewBegin(int beg) {
+        if (beg > min_start) {
+            min_start = beg;
+            check();
+        }
+    }
 
-    void check();
+    void setNewEnd(int end) {
+        if (max_start + size > end) {
+            max_start = end - size;
+            check();
+        }
+    }
+
+    void check() {
+        int cnt = 0;
+        //int idx = min_start;
+        for (int i = min_start; i <= max_start + size; ++i) {
+            if (fields_prt[i] == INON::COLOR_WHITE) {
+                cnt = 0;
+                continue;
+            }
+            cnt++;
+            if (cnt == size) {
+                if (i != min_start + size) {
+                    min_start = i - size;
+                }
+                break;
+            }
+        }
+        cnt = 0;
+        for (int i = max_start + size; i >= min_start; --i) {
+            if (fields_prt[i] == INON::COLOR_WHITE) {
+                cnt = 0;
+                continue;
+            }
+            cnt++;
+            if (cnt == size) {
+                if (i != max_start - size) {
+                    min_start = i - size;
+                }
+                break;
+            }
+        }
+    }
 };
 
 void Obj::check() {
